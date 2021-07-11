1eae2a128f4cecbc654d0f79d1e54244cee71484
lavakush7800 <lavakushgiri@gmail.com>
2021-07-11 20:40:41 +0530

    user sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLiistComponent } from './user-liist/user-liist.component';
 import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLiistComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
